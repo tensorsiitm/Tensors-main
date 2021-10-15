c380f204495ecc86c972db99a7db610eb876834d
Firoz-PS <ce19b048@smail.iitm.ac.in>
2021-10-15 12:11:43 +0530

    Updates

---
+++ component---src-pages-team-2021-22-js-2e5eea2c51e8196369d4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{VgaG:function(e,t,a){"use strict";a.r(t),a.d(t,"default",(function(){return i}));var n=a("q1tI"),l=a.n(n),r=(a("4l1m"),a("/KGT"),a("HP2+"),a("t4Y7")),c=a("vrFN"),u=a("Bl7J"),o=a("CLjq");a("WHHo");function i(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{page:"team"},l.a.createElement(c.a,{title:"Team 2021-22"}),l.a.createElement(o.a,null)))}}}]);
+//# sourceMappingURL=component---src-pages-team-2021-22-js-2e5eea2c51e8196369d4.js.map
